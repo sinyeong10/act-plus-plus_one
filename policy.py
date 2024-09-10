eba785efe4c45b2cbd36fcc73b57297213bd3b10
V. hs Sin <cbrnt1210@naver.com>
2024-09-11 08:57:45 +0900

    mycobot320안쓰는라이브러리주석

--- policy.py
+++ policy.py
@@ -8,13 +8,13 @@ import IPython
 e = IPython.embed
 
 from collections import OrderedDict
-from robomimic.models.base_nets import ResNet18Conv, SpatialSoftmax
-from robomimic.algo.diffusion_policy import replace_bn_with_gn, ConditionalUnet1D
+# from robomimic.models.base_nets import ResNet18Conv, SpatialSoftmax
+# from robomimic.algo.diffusion_policy import replace_bn_with_gn, ConditionalUnet1D
 
 
-from diffusers.schedulers.scheduling_ddpm import DDPMScheduler
-from diffusers.schedulers.scheduling_ddim import DDIMScheduler
-from diffusers.training_utils import EMAModel
+# from diffusers.schedulers.scheduling_ddpm import DDPMScheduler
+# from diffusers.schedulers.scheduling_ddim import DDIMScheduler
+# from diffusers.training_utils import EMAModel
 
 
 class DiffusionPolicy(nn.Module):
